e48d17c02418f5d59e4fb18796a32a132dc80fb0
robertkachniarz <kachniarz.robert@gmail.com>
2022-07-01 21:13:02 +0200

    Create view of single car

--- app/src/app/car/view/view.component.ts
+++ app/src/app/car/view/view.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarService } from '../car.service';
+import { Car } from '../car';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
+  styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
+  id!: string;
+  car!: Car;
 
-  constructor() { }
+  constructor(
+    public carService: CarService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.id = this.route.snapshot.params['carId'];
 
+    this.carService.find(this.id).subscribe((dane: Car) => {
+      this.car = dane;
+    });
+  }
 }
